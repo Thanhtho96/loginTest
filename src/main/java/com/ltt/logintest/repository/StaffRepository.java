a43940e2a32f2bd9acf170c7c5bffcf2749705bc
thanhbat96 <thanhbat96@gmail.com>
2019-05-15 11:03:43 +0700

    update

--- src/main/java/com/ltt/logintest/repository/StaffRepository.java
+++ src/main/java/com/ltt/logintest/repository/StaffRepository.java
@@ -11,15 +11,18 @@ import java.util.List;
 public interface StaffRepository extends CrudRepository<Staff, Long> {
     List<Staff> findByName(String name);
 
-    @Query("from Staff st where st.name like %:name% " +
-            "and (:startDate is null or st.birthday >=:startDate) and (:endDate is null or st.birthday <= :endDate) " +
-            "and st.phonenumber like %:phonenumber% and st.address like %:address%")
+//    @Query("from Staff st where st.name like %:name% " +
+//            "and (:startDate is null or st.birthday >=:startDate) and (:endDate is null or st.birthday <= :endDate) " +
+//            "and st.phonenumber like %:phonenumber% and st.address like %:address%")
+    @Query(value = "select * from staff where name like %:name%" +
+            "and (:startDate is null birthday >= :startDate)" +
+            "and (:endDate is null birthday <= :endDate)" +
+            "and phonenumber like :phonenumber and address like :address", nativeQuery = true)
     List<Staff> searchByProperties(@Param("name") String name,
                                    @Param("startDate") Date birthday,
                                    @Param("endDate") Date endDate,
                                    @Param("phonenumber") String phonenumber,
-                                   @Param("address") String address);
-
+                                    @Param("address") String address);
 
     @Query("from Staff st where st.name like %:name% " +
             "and (concat(st.birthday, '') like %:birthday%) and st.phonenumber like %:phonenumber% and st.address like %:address%")
